f67d6c22cca111d89ffc16adddde4c1cc81ff708
Tom Hutcheson <thomasjhutch@gmail.com>
2019-12-22 19:07:02 -0600

    made the game work. cant get it to have a hint. or track wins and loses.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,23 +2,20 @@ window.onload = function () {
 
     var alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
         'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
-        'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+        'T', 'U', 'V', 'W', 'X', 'Y', 'Z', "1", "2", "3", "4", "5", "6"];
 
-    var categories;         // Array of topics
-    var chosenCategory;     // Selected catagory
-    var getHint;          // Word getHint
-    var word;              // Selected word
-    var guess;             // Geuss
-    var geusses = [];      // Stored geusses
+    var rebel;               // Array of topics
+    var chosenCategory;     // Selected category
+    var word;              // Selected wrd
+    var guess;             // guess
+    var guesses = [];      // Stored guesses
     var lives;             // Lives
-    var counter;           // Count correct geusses
-    var space;              // Number of spaces in word '-'
+    var counter;           // Count correct guesses
+    var space;              // Number of spaces in wrd '-'
 
     // Get elements
     var showLives = document.getElementById("mylives");
-    var showCatagory = document.getElementById("scatagory");
-    var getHint = document.getElementById("hint");
-    var showClue = document.getElementById("clue");
+    var showCategory = document.getElementById("hint");
 
 
 
@@ -39,18 +36,7 @@ window.onload = function () {
     }
 
 
-    // Select Catagory
-    var selectCat = function () {
-        if (chosenCategory === categories[0]) {
-            catagoryName.innerHTML = "The Chosen Category Is Premier League Football Teams";
-        } else if (chosenCategory === categories[1]) {
-            catagoryName.innerHTML = "The Chosen Category Is Films";
-        } else if (chosenCategory === categories[2]) {
-            catagoryName.innerHTML = "The Chosen Category Is Cities";
-        }
-    }
-
-    // Create geusses ul
+    // Create guesses ul
     result = function () {
         wordHolder = document.getElementById('hold');
         correct = document.createElement('ul');
@@ -66,113 +52,46 @@ window.onload = function () {
                 guess.innerHTML = "_";
             }
 
-            geusses.push(guess);
+            guesses.push(guess);
             wordHolder.appendChild(correct);
             correct.appendChild(guess);
         }
     }
 
+    // give a hint 
+
+
     // Show lives
     comments = function () {
         showLives.innerHTML = "You have " + lives + " lives";
         if (lives < 1) {
-            showLives.innerHTML = "Game Over";
+            showLives.innerHTML = "I find your lack of faith disturbing!";
         }
-        for (var i = 0; i < geusses.length; i++) {
-            if (counter + space === geusses.length) {
-                showLives.innerHTML = "You Win!";
+        for (var i = 0; i < guesses.length; i++) {
+            if (counter + space === guesses.length) {
+                showLives.innerHTML = "You saved your Rebel friend!";
             }
         }
     }
-
-    // Animate man
-    var animate = function () {
-        var drawMe = lives;
-        drawArray[drawMe]();
-    }
-    // Hangman
-    canvas = function () {
-
-        myStickman = document.getElementById("stickman");
-        context = myStickman.getContext('2d');
-        context.beginPath();
-        context.strokeStyle = "#fff";
-        context.lineWidth = 2;
-    };
-
-    head = function () {
-        myStickman = document.getElementById("stickman");
-        context = myStickman.getContext('2d');
-        context.beginPath();
-        context.arc(60, 25, 10, 0, Math.PI * 2, true);
-        context.stroke();
-    }
-
-    draw = function ($pathFromx, $pathFromy, $pathTox, $pathToy) {
-
-        context.moveTo($pathFromx, $pathFromy);
-        context.lineTo($pathTox, $pathToy);
-        context.stroke();
-    }
-
-    frame1 = function () {
-        draw(0, 150, 150, 150);
-    };
-
-    frame2 = function () {
-        draw(10, 0, 10, 600);
-    };
-
-    frame3 = function () {
-        draw(0, 5, 70, 5);
-    };
-
-    frame4 = function () {
-        draw(60, 5, 60, 15);
-    };
-
-    torso = function () {
-        draw(60, 36, 60, 70);
-    };
-
-    rightArm = function () {
-        draw(60, 46, 100, 50);
-    };
-
-    leftArm = function () {
-        draw(60, 46, 20, 50);
-    };
-
-    rightLeg = function () {
-        draw(60, 70, 100, 100);
-    };
-
-    leftLeg = function () {
-        draw(60, 70, 20, 100);
-    };
-
-    drawArray = [rightLeg, leftLeg, rightArm, leftArm, torso, head, frame4, frame3, frame2, frame1];
-
 
 
 
     // OnClick Function
     check = function () {
         list.onclick = function () {
-            var geuss = (this.innerHTML);
+            var guess = (this.innerHTML);
             this.setAttribute("class", "active");
             this.onclick = null;
             for (var i = 0; i < word.length; i++) {
-                if (word[i] === geuss) {
-                    geusses[i].innerHTML = geuss;
+                if (word[i] === guess) {
+                    guesses[i].innerHTML = guess;
                     counter += 1;
                 }
             }
-            var j = (word.indexOf(geuss));
+            var j = (word.indexOf(guess));
             if (j === -1) {
                 lives -= 1;
                 comments();
-                animate();
             } else {
                 comments();
             }
@@ -182,37 +101,25 @@ window.onload = function () {
 
     // Play
     play = function () {
-        categories = [
-            ["Luke Skywalker", "Mon Mothma", "Leia Organa", "Admiral Ackbar",
-                "Han Solo", "Chewbacca", "R2-D2", "C-3PO", "Lando", "Obi-Wan"],
-
+        rebel = [
+            ["LUKE SKYWALKER", "MON MOTHMA", "LEIA ORGANA", "ADMIRAL ACKBAR",
+                "HAN SOLO", "CHEWBACCA", "R2-D2", "C-3PO", "LANDO", "OBI WAN"],
         ];
 
-        chosenCategory = categories[Math.floor(Math.random() * categories.length)];
+        chosenCategory = rebel[Math.floor(Math.random() * rebel.length)];
         word = chosenCategory[Math.floor(Math.random() * chosenCategory.length)];
         word = word.replace(/\s/g, "-");
         console.log(word);
         buttons();
 
-        geusses = [];
+        guesses = [];
         lives = 10;
         counter = 0;
         space = 0;
         result();
         comments();
-        selectCat();
-        canvas();
     }
 
     play();
 
-    // Reset
-
-    document.getElementById('reset').onclick = function () {
-        correct.parentNode.removeChild(correct);
-        letters.parentNode.removeChild(letters);
-        showClue.innerHTML = "";
-        context.clearRect(0, 0, 400, 400);
-        play();
-    }
 }
